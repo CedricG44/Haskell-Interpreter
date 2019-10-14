0e8113e74319fef594ce56ff3be69e715a6a1470
batnyu <vrignaudbaptiste@gmail.com>
2019-10-14 15:37:39 +0200

    Haskell TP2 if-then-else

--- src/main/resources/TP2.hs
+++ src/main/resources/TP2.hs
@@ -1,6 +1,10 @@
 -- on generalise (autant que possible) le type des fonctions du bloc1
 
+--cons a b = \a -> \b -> \f -> ((f a) b)
+
 myHead :: [a] -> a
+--myHead xs = (\c -> (c (\ a b -> a))) xs
+--myHead _ = undefined
 myHead (x:_) = x
 
 myTail :: [a] -> [a]
@@ -10,11 +14,13 @@ myAppend :: [a] -> [a] -> [a]
 myAppend xs ys = myAppend' xs 
     where --myAppend' :: [a] -> [a]
           myAppend' (x:xs) = x:myAppend' xs
-          myAppend' [] = ys  
+          myAppend' [] = ys
 
 myInit :: [a] -> [a]
-myInit [_] = []
-myInit (x:xs) = x:(myInit xs)
+myInit xs = 
+    if length xs <= 1 
+        then []
+    else (head xs):(myInit (tail xs))
 
 myLast :: [a] -> a
 myLast [x] = x
@@ -25,43 +31,56 @@ myNull [] = True
 myNull _ = False
 
 myLength :: [a] -> Int
-myLength (_:xs) = 1 + myLength xs
-myLength [] = 0
+myLength list =
+    if myNull list
+        then 0
+    else 1 + myLength (myTail list)
 
 myReverse :: [a] -> [a]
-myReverse (x:xs) = myAppend (myReverse xs) [x]
-myReverse xs = xs
+myReverse list = 
+    if myNull list
+        then list
+    else myAppend (myReverse (myTail list)) [myHead list]
 
 myConcat :: [[a]] -> [a]
 myConcat (xs:xss) = xs ++ myConcat xss
 myConcat [] = []
 
 myTake :: Int -> [b] -> [b]
-myTake 0 _  = []
-myTake n [] = []
-myTake n (x:xs) = x:myTake (n-1) xs
+myTake index list =
+    if index <= 0
+        then []
+    else if myNull list
+        then []
+    else myHead list : myTake (index-1) (myTail list)
 
 myDrop :: Int -> [element] -> [element]
-myDrop 0 xs = xs
-myDrop n [] = []
-myDrop n (x:xs) = myDrop (n-1) xs
+myDrop index list =
+    if index <= 0
+        then list
+    else if myNull list
+        then []
+    else myDrop (index-1) (myTail list)
 
 myBangBang :: [a] -> Int -> a
-myBangBang (x:xs) 0 = x
-myBangBang (x:xs) n = myBangBang xs (n-1)
+myBangBang list index = 
+    if index <= 0
+        then myHead list 
+    else myBangBang (myTail list) (index-1)
 
 myInsert :: Ord a => a -> [a] -> [a]
-myInsert x [] = [x]
-myInsert x (y:ys) | x>y       = y:myInsert x ys
-                  | otherwise = x:y:ys
-
-toto :: Eq a => [a] -> Bool
-toto xs = xs==[]
+myInsert element list =
+    if myNull list
+        then [element]
+    else if element > myHead list
+        then myHead list : myInsert element (myTail list)
+    else element : myHead list : myTail list
 
 mySort :: Ord a => [a] -> [a]
---mySort :: [a] -> [a]
-mySort (x:xs) = myInsert x (mySort xs)
-mySort [] = []
+mySort list =
+    if myNull list
+        then []
+    else myInsert (myHead list) (mySort (myTail list))
 
 -- NEW STUFF
 -- ordre superieur
@@ -76,24 +95,30 @@ myCompose :: (b -> c) -> (a -> b) -> a -> c
 myCompose f g x = f (g x)
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f (x:xs) = f x:myMap f xs
-myMap f []     = []
+myMap f list =
+    if myNull list
+        then []
+    else f (myHead list) : myMap f (myTail list)
 
 test1 = myMap odd [1..10]
 
 -- calcul des sous liste en utilisant map
 
 sousListes :: [a] -> [[a]]
-sousListes (x:xs) = s ++ map (x:) s where s = sousListes xs
-sousListes [] = [[]]
+sousListes list =
+    if myNull list
+        then [[]]
+    else s ++ map ((myHead list):) s where s = sousListes (myTail list)
 
 -- une fonction plus generale: foldr
 -- inferer le type de foldr
 -- forme graphique de la liste en peigne
 
-myFoldr :: (a -> b -> b) -> b -> [a] -> b 
-myFoldr f k (x:xs) = f x (myFoldr f k xs)
-myFoldr f k []     = k
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f k list =
+    if myNull list
+        then k
+    else f (myHead list) (myFoldr f k (myTail list))
 
 myAnd' :: [Bool] -> Bool
 myAnd' = myFoldr (&&) True 
@@ -136,9 +161,12 @@ myFst (x,_) = x
 -- TODO: definir recursivement
 
 myDropWhile :: (a -> Bool) -> [a] -> [a]
-myDropWhile f (x:xs) | f x       = myDropWhile f xs
-                     | otherwise = (x:xs)
-myDropWhile f []     = []
+myDropWhile f list =
+    if myNull list
+        then []
+    else if f (myHead list)
+        then myDropWhile f (myTail list)
+    else (myHead list : myTail list)
 
 myElem :: Eq a => a -> [a] -> Bool
 myElem x (y:ys) | x == y    = True
